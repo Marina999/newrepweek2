2ff51b3c9b5368146ada270f4d092a5e28f4bc60
marina999 <Maryam@192.168.5.102>
2015-09-03 13:51:16 -0700

    files

---
+++ classwork1/src/classwork1/testdummie.java
@@ -0,0 +1,12 @@
+package classwork1;
+
+public class testdummie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World!!");
+
+	}
+	
+
+}
